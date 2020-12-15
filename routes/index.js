f870ac8879f4a31048151352fcd18fe41c74d841
jakk43 <jakkapong314@gmail.com>
2020-12-15 17:57:25 +0700

    show d

--- routes/index.js
+++ routes/index.js
@@ -51,18 +51,18 @@ router.post('/user_sign_up', function (req, res) {
 	var sql = "insert into users(user_name,email,password,confirm_password)" //care number
 	sql += " values('" + name + "','" + email + "','" + pw + "','" + cpw + "')";
 
-		if(pw===cpw){
-      con.query(sql, function (err, result) {
-        if (err) {
-          res.send("user name is already in use ");
-        } else {
-          console.log(result)
-          res.send("Successful")
-        }
-      })
-    }else{
-      res.send("passowrd not match")
-    }
+	if (pw === cpw) {
+		con.query(sql, function (err, result) {
+			if (err) {
+				res.send("user name is already in use ");
+			} else {
+				console.log(result)
+				res.send("Successful")
+			}
+		})
+	} else {
+		res.send("passowrd not match")
+	}
 
 });
 
@@ -98,21 +98,33 @@ router.post('/contact', function (req, res) {
 	var sql = "insert into contact(email,details)" //care number
 	sql += " values('" + email + "','" + details + "')";
 	// console.log(email)
+
+if((email && details )!= ''){
 	con.query(sql, function (err, result) {
-        if (err) {
-          res.send("ส่งไม่สำเร็จ");
-        } else {
-        //   console.log(result)
-          res.send("ส่งแล้ว")
-        }
-      })
+		if (err) {
+			res.send("ส่งไม่สำเร็จ");
+		} else {
+			//   console.log(result)
+			res.send("ส่งแล้ว")
+		}
+	})
+}else{
+	res.send("กรอกข้อมูลก่อน อส")
+}
+
+	
 
 });
 
 // ดึงข้อมูล Contact ออกมา
-router.get('/usercontact',function(req,res,next){
-	con.query('SELECT * FROM contact',function(err,rows){
-		res.send(rows);
+router.get('/usercontact', function (req, res, next) {
+	con.query('SELECT * FROM contact', function (err, rows) {
+		// res.send(rows[2].email);
+		var datadetails = '';
+		for (let i = 0; i < rows.length; i++) {
+			datadetails += String(rows[i].no) + "." + rows[i].email + " | " + rows[i].details + "<br>"
+		}
+		res.send(datadetails)
 	})
 })
 module.exports = router;
